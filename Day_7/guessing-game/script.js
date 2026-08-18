76d448fbadd2a30ce7c04c2024ed014d2b7f2d2e
Landry-tml <landrytemolong03@gmail.com>
2026-08-18 15:34:37 +0100

    Grab references to every HTML element you'll touch

--- Day_7/guessing-game/script.js
+++ Day_7/guessing-game/script.js
@@ -4,3 +4,10 @@ let attempts = 0;
 let hasWon = false;
 let gameOver = false;
 
+const guessInput = document.getElementById("guessInput");
+const guessBtn = document.getElementById("guessBtn");
+const messageEl = document.getElementById("message");
+const attemptsEl = document.getElementById("attemptsLeft");
+const statusEl = document.getElementById("status");
+const historyEl = document.getElementById("history");
+const againBtn = document.getElementById("againBtn");
