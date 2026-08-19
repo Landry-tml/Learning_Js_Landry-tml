71c2383254861602f4830e719725fb088703576a
Landry-tml <landrytemolong03@gmail.com>
2026-08-19 16:39:37 +0100

    Grabbed the DOM references

--- Day_9/script.js
+++ Day_9/script.js
@@ -1,2 +1,13 @@
 let cart = [];
 
+const itemNameInput = document.getElementById("itemName");
+const itemPriceInput = document.getElementById("itemPrice");
+const priorityCheck = document.getElementById("priorityCheck");
+const addBtn = document.getElementById("addBtn");
+const removeFirstBtn = document.getElementById("removeFirstBtn");
+const removeLastBtn = document.getElementById("removeLastBtn");
+const previewBtn = document.getElementById("previewBtn");
+const previewBox = document.getElementById("previewBox");
+const itemsList = document.getElementById("itemsList");
+const emptyMsg = document.getElementById("emptyMsg");
+const totalValue = document.getElementById("totalValue");
