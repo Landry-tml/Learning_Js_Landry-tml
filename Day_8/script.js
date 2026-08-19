a947534b9003db345e384ef3c192b77cf13cbcc8
Landry-tml <landrytemolong03@gmail.com>
2026-08-19 12:16:59 +0100

    Wrote one function per shape

---
+++ Day_8/script.js
@@ -0,0 +1,12 @@
+function areaOfCircle(radius) {
+  return Math.PI * radius * radius;
+}
+function areaOfRectangle(width, height) {
+  return width * height;
+}
+function areaOfTriangle(base, height) {
+  return 0.5 * base * height;
+}
+function areaOfSquare(side) {
+  return side * side;
+}
