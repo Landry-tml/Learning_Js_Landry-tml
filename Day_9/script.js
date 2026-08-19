f41f9e4db8ced6847d30866d450383b5c6add9b6
Landry-tml <landrytemolong03@gmail.com>
2026-08-19 16:42:51 +0100

    Wrote the add item function, push,unshift,or merge quantity

--- Day_9/script.js
+++ Day_9/script.js
@@ -11,3 +11,16 @@ const previewBox = document.getElementById("previewBox");
 const itemsList = document.getElementById("itemsList");
 const emptyMsg = document.getElementById("emptyMsg");
 const totalValue = document.getElementById("totalValue");
+
+function addItem(name, price, priority) {
+  const existingIndex = cart.findIndex(item => item.name.toLowerCase() === name.toLowerCase());
+  
+  if (existingIndex !== -1) {
+    cart[existingIndex].qty += 1;
+  } else if (priority) {
+    cart.unshift({ name, price, qty: 1 });
+  } else {
+    cart.push({ name, price, qty: 1 });
+  }
+  renderCart();
+}
