6f6c7d55c4acff037b483479a986601e9eafb37b
Landry-tml <landrytemolong03@gmail.com>
2026-08-19 23:36:58 +0100

    Added a shared method via the prototype

--- Day_10/script.js
+++ Day_10/script.js
@@ -4,3 +4,7 @@ function Contact(id, name, phone, email) {
   this.phone = phone;
   this.email = email;
 }
+
+Contact.prototype.getInitials = function () {
+  return this.name.split(" ").map(part => part[0]).join("").slice(0, 2).toUpperCase();
+};
